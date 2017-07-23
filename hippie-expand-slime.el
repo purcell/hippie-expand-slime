39bbae94896a62854d31754debdfae71d35fec62
Steve Purcell <steve@sanityinc.com>
2017-07-23 13:46:08 +1200

    Add licence info (GPL)
    Fixes https://github.com/purcell/flymake-coffee/issues/11

--- hippie-expand-slime.el
+++ hippie-expand-slime.el
@@ -1,8 +1,23 @@
 ;;; hippie-expand-slime.el --- Hook slime's completion into hippie-expand
 
+;; Copyright (c) 2015-2017 Steve Purcell
+
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; URL: https://github.com/purcell/hippie-expand-slime
-;; Version: DEV
+;; Package-Version: 0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
